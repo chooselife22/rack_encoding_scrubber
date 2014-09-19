2ecdf610b147394913dbd3d33ddd06b29ad50c77
Stefan Wienert <stefan.wienert@pludoni.de>
2014-09-19 19:46:02 +0200

    Removes non utf-8 encoded parts from the url

--- lib/rack_encoding_scrubber.rb
+++ lib/rack_encoding_scrubber.rb
@@ -1,6 +1,8 @@
 require "rack_encoding_scrubber/version"
 
 class RackEncodingScrubber
+  REGEX_UTF = /%u00\h{0,2}/
+  REGEX_MB  = /%[a-fA-F]\h/
   def initialize(app)
     @app = app
   end
@@ -14,9 +16,19 @@ class RackEncodingScrubber
     request_method = env['REQUEST_METHOD']
     if request_method == 'GET'
       %w[QUERY_STRING REQUEST_PATH PATH_INFO QUERY_STRING REQUEST_URI ORIGINAL_FULLPATH].each do |header|
-        if env[header] and env[header]["%u00"]
-          env[header].gsub!(/%u00\h{0,2}/, "")
-          env[header].gsub!(/%\h{0,2}/,"")
+        if h = env[header]
+          if h["%u00"]
+            h.gsub!(REGEX_UTF, "")
+          end
+          if h[REGEX_MB] # check for byte
+            tmp = CGI.unescape(h).force_encoding('utf-8')
+            if !tmp.valid_encoding?
+              env[header] = CGI.escape(tmp.scrub(''))
+              if %w[REQUEST_PATH PATH_INFO REQUEST_URI].include? header
+                env[header].gsub! '%2F', '/'
+              end
+            end
+          end
         end
       end
     end
